755d9e3bdbecd6834213c2e96835b462c56e93cd
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2025-06-14 13:49:03 +0700

    update port

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(4000, () => console.log("Server running on port 4000"));
